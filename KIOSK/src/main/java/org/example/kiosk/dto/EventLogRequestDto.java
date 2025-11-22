2acd40af10a407e441c4845bb4e0abf63cc1433d
JaeHyeok <yang55923@gmail.com>
2025-11-22 22:43:57 +0900

    dto 설명 추가

--- KIOSK/src/main/java/org/example/kiosk/dto/EventLogRequestDto.java
+++ KIOSK/src/main/java/org/example/kiosk/dto/EventLogRequestDto.java
@@ -1,11 +1,26 @@
 package org.example.kiosk.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
-
 import java.util.Map;
 
 @Getter
 public class EventLogRequestDto { // 이벤트 로그 저장 dto
+
+  @Schema(
+          description = "이벤트 이름",
+          example = "STAY_TIME"
+  )
   private String eventName;
+
+  @Schema(
+          description = "이벤트 상세 데이터",
+          example = """
+            {
+              "page": "main-menu",
+              "staySeconds": 12
+            }
+            """
+  )
   private Map<String, Object> payLoad;
 }
