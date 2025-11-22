fa3f69de4372883e8ebc01ec6430b0d9b0e8456f
JaeHyeok <yang55923@gmail.com>
2025-11-23 03:31:38 +0900

    dto 예제 수정

--- KIOSK/src/main/java/org/example/kiosk/dto/EventLogRequestDto.java
+++ KIOSK/src/main/java/org/example/kiosk/dto/EventLogRequestDto.java
@@ -17,7 +17,7 @@ public class EventLogRequestDto { // 이벤트 로그 저장 dto
           description = "이벤트 상세 데이터",
           example = """
             {
-              "userCount" : 1
+              "userCount" : 1,
               "pageNum": 3,
               "staySeconds": 12
             }
